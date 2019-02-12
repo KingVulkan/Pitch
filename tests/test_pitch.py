80e3fb496ac69a9a14204a84120b423e38141bf4
KingVulkan <manowfelow@gmail.com>
2019-02-12 13:42:06 +0300

    adds a test file

---
+++ tests/test_pitch.py
@@ -0,0 +1,28 @@
+import unittest
+from app.models import Pitch,User
+from app import db
+
+class PitchModelTest(unittest.TestCase):
+    def setUp(self):
+        self.user_manow = User(username = 'manow',password = '1234')
+        self.new_pitch = Pitch(name='cat',title='movie',description='moviereview',user =self.user_manow, category='technology')
+
+    # def tearDown(self):
+    #     Pitch.query.delete()
+    #     User.query.delete()
+
+    def test_check_instance_variable(self):
+        self.assertEquals(self.new_pitch.name,'cat')
+        self.assertEquals(self.new_pitch.title,'movie')
+        self.assertEquals(self.new_pitch.description,'moviereview')
+        self.assertEquals(self.new_pitch.category, 'technology')
+        # self.assertEquals(self.new_pitch.user,self.user_manow)  
+
+    def test_save_pitch(self):
+        self.new_pitch.save_pitch()
+        self.assertTrue(len(Pitch.query.all()) >0)
+
+    def test_get_pitch_by_id(self):
+        self.new_pitch.save_pitch()
+        got_pitch = Pitch.get_pitches(12345)
+        self.assertTrue(len(got_pitch) > 0)    
