38c6e91d856ed5e5fd3588fbad5ac55898b3f0a4
KingVulkan <manowfelow@gmail.com>
2019-02-12 16:36:08 +0300

    adds  tests in pitch

--- tests/test_pitch.py
+++ tests/test_pitch.py
@@ -5,17 +5,17 @@ from app import db
 class PitchModelTest(unittest.TestCase):
     def setUp(self):
         self.user_manow = User(username = 'manow',password = '1234')
-        self.new_pitch = Pitch(name='cat',title='movie',description='moviereview',user =self.user_manow, category='technology')
+        self.new_pitch = Pitch(name='zee',title='Money',description='moneyreview',user =self.user_manow, category='Finance')
 
     # def tearDown(self):
     #     Pitch.query.delete()
     #     User.query.delete()
 
     def test_check_instance_variable(self):
-        self.assertEquals(self.new_pitch.name,'cat')
-        self.assertEquals(self.new_pitch.title,'movie')
-        self.assertEquals(self.new_pitch.description,'moviereview')
-        self.assertEquals(self.new_pitch.category, 'technology')
+        self.assertEquals(self.new_pitch.name,'zee')
+        self.assertEquals(self.new_pitch.title,'Money')
+        self.assertEquals(self.new_pitch.description,'moneyreview')
+        self.assertEquals(self.new_pitch.category, 'Finance')
         # self.assertEquals(self.new_pitch.user,self.user_manow)  
 
     def test_save_pitch(self):
